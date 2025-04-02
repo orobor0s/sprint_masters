d4084e6d3bdb321c4c7f105c3ca5d93fa10151c5
Ben <benjanac.dev@gmail.com>
2025-04-01 22:29:31 -0400

    standardized formatting, implemented parameters for the EONET API call, which included generating dictionaries with information about event sources and categories

--- application/main.py
+++ application/main.py
@@ -1,148 +1,155 @@
+from datetime import datetime, timedelta
 import json
 import requests
 import urllib.parse
 
-def getIPData():
-    '''Fetch the client's IP data'''
 
-    # Need a User-Agent header in the request to circumvent rate limiting
+# Variables
+ipapi_url = "https://ipapi.co/json"
+eonet_source_url = "https://eonet.gsfc.nasa.gov/api/v3/sources"
+eonet_categories_url = "https://eonet.gsfc.nasa.gov/api/v3/categories"
+eonet_query_url = "https://eonet.gsfc.nasa.gov/api/v3/events/geojson?"
+default_start_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
+default_end_date = datetime.today().strftime('%Y-%m-%d')
+
+
+# Utility
+def is_float(n):
+    '''Check if string is float'''
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
+
+def is_valid_date(date_str):
+    '''Determines if a date is valid'''
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")  # Attempt to parse
+        return True
+    except ValueError:
+        return False
+
+def wrap_lon(lon):
+    '''Wraps longitude to support a bbox that crosses the international date line'''
+    return ((lon + 180) % 360) - 180
+
+def calc_bbox(scale):
+    '''Calculates coordinate pair to bound bbox based on the client's location and scale setting'''
+    lon, lat = client_data["longitude"], client_data["latitude"]
+    min_lon, max_lon = wrap_lon(lon - scale), wrap_lon(lon + scale)
+    min_lat, max_lat = max(-90, lat - scale), min(90, lat + scale)
+    return ",".join(map(str, [min_lon, max_lat, max_lon, min_lat]))
+
+
+# Input sanitization
+def sanitize_list_input(input,keyword):
+    '''Sanitize item and category inputs'''
+    if input:
+        valid = []
+        errors = []
+        if keyword == "sources":
+            input = input.upper()
+            key = "source"
+        else:
+            key = "category"
+        input_check = input.split(',')
+        for item in input_check:
+            if item in globals().get(keyword, "Variable not found"):
+                valid.append(item)
+            else:
+                errors.append(item)
+        if errors:
+                print(f"{keyword} input errors: {errors}")
+        if valid:
+            return {key: ",".join(valid)}
+        else:
+            return {}
+    else:
+        return {}
+
+def sanitize_status(status):
+    '''Sanitize status input'''
+    return {"status": status} if status in {"open", "closed", "all"} else {}
+
+def sanitize_limit(limit):
+    '''Sanitize limit input'''
+    return {"limit": limit} if limit.isdigit() else {}
+
+def sanitize_date_range(start, end):
+    '''Sanitize date range input'''
+    if is_valid_date(start) and is_valid_date(end) and end >= start:
+        return {"start": start, "end": end}
+    return {}
+
+def sanitize_scale(scale):
+    '''Sanitize scale input'''
+    if is_float(scale) and float(scale) >= 0:
+        return {"bbox": calc_bbox(scale)}
+    return {}
+
+
+# EONET enabling infrastructure
+def generate_eonet_dictionaries(url,keyword):
+    '''Generates a dictionary that contains eonet sources or categories'''
+    eonet_data = get_eonet_data(url)[keyword]
+    data = {}
+    for item in eonet_data:
+        item_id = item["id"]
+        del item["id"]
+        data[item_id] = item
+    return data
+
+def generate_eonet_query(source="",category="",status="",limit="",start=default_start_date,end=default_end_date,scale=10):
+    '''Generates EONET query url based on sanitized user input'''
+    # Set parameters
+    params = {}
+    params.update(sanitize_list_input(source,"sources"))
+    params.update(sanitize_list_input(category,"categories"))
+    params.update(sanitize_status(status))
+    params.update(sanitize_limit(limit))
+    params.update(sanitize_date_range(start, end))
+    params.update(sanitize_scale(scale))
+    
+    # Generate API query
+    query_string = urllib.parse.urlencode(params, quote_via=urllib.parse.quote, safe=",")
+    return f"{eonet_query_url}{query_string}"
+
+
+# API queries
+def get_ip_data():
+    '''Gets the client's IP data'''
+    # Needs a User-Agent header in the request to circumvent rate limiting
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
-    replydata = requests.get("https://ipapi.co/json/", headers=headers)
-    json_data = replydata.json()
-    json_status = replydata.status_code
+    reply_data = requests.get(f"{ipapi_url}", headers=headers)
+    json_data = reply_data.json()
+    json_status = reply_data.status_code
 
     if json_status == 200:
         return json_data
 
-def wrapLon(lon):
-    '''Wrap longitude to support bboxes that cross the international date line'''
-    return ((lon + 180) % 360) - 180
-
-def getEONETData(scale=10):
-    '''Fetch EONET event data based on the client's location and customized parameters'''
-
-    # Calculate coordinate pair to bound bbox
-    lon, lat = clientData["longitude"], clientData["latitude"]
-    min_lon, max_lon = wrapLon(lon - scale), wrapLon(lon + scale)
-    min_lat, max_lat = max(-90, lat - scale), min(90, lat + scale)
-    bbox = ",".join(map(str, [min_lon, max_lat, max_lon, min_lat]))
-
-    # Generate API query
-    params = {"bbox":bbox}
-    query_string = urllib.parse.urlencode(params, quote_via=urllib.parse.quote, safe=",")
-    url = f"https://eonet.gsfc.nasa.gov/api/v3/events?{query_string}"
-    
-    replydata = requests.get(url)
-    json_data = replydata.json()
-    json_status = replydata.status_code
+def get_eonet_data(url):
+    '''Gets EONET event data based on the client's location and customized parameters'''
+    reply_data = requests.get(url)
+    json_data = reply_data.json()
+    json_status = reply_data.status_code
 
     if json_status == 200:
        return json_data
     else:
         print("Error message: " + json_data["message"])
 
-clientData = getIPData()
-if clientData != None:
-    print(json.dumps(getEONETData(), indent=4))
 
-# route_url = "https://graphhopper.com/api/1/route?"
-# key = "9b36e834-2f7d-494e-bc6c-bee36b11090e"
-# 
-# def geocoding(location, key):
-#     while location == "":
-#         location = input("Enter the location again: ")
-#     geocode_url = "https://graphhopper.com/api/1/geocode?"
-#     url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1", "key":key})
-# 
-#     replydata = requests.get(url)
-#     json_data = replydata.json()
-#     json_status = replydata.status_code
-# 
-#     if json_status == 200 and len(json_data["hits"]) != 0:
-#         json_data = requests.get(url).json()
-#         lat = json_data["hits"][0]["point"]["lat"]
-#         lng = json_data["hits"][0]["point"]["lng"]
-#         name = json_data["hits"][0]["name"]
-#         value = json_data["hits"][0]["osm_value"]
-#         
-#         if "country" in json_data["hits"][0]:
-#             country = json_data["hits"][0]["country"]
-#         else:
-#             country = ""
-# 
-#         if "state" in json_data["hits"][0]:
-#             state = json_data["hits"][0]["state"]
-#         else:
-#             state = ""
-# 
-#         if len(state) != 0 and len(country) != 0:
-#             new_loc = name + ", " + state + ", " + country
-#         elif len(state) != 0:
-#             new_loc = name + ", " + country
-#         else:
-#             new_loc = name
-# 
-#         print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n" + url)
-#     else:
-#         lat = "null"
-#         lng = "null"
-#         new_loc = location
-#         if json_status != 200:
-#             print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"])
-#     return json_status,lat,lng,new_loc
-# 
-# while True:
-#     print("\n+++++++++++++++++++++++++++++++++++++++++++++")
-#     print("Vehicle profiles available on Graphhopper:")
-#     print("+++++++++++++++++++++++++++++++++++++++++++++")
-#     print("car, bike, foot")
-#     print("+++++++++++++++++++++++++++++++++++++++++++++")
-#     profile=["car", "bike", "foot"]
-#     vehicle = input("Enter a vehicle profile from the list above: ")
-#     if vehicle == "quit" or vehicle == "q":
-#         break
-#     elif vehicle in profile:
-#         vehicle = vehicle
-#     else:
-#         vehicle = "car"
-#         print("No valid vehicle profile was entered. Using the car profile.")
-#     loc1 = input("Starting Location: ")
-#     if loc1 == "quit" or loc1 == "q":
-#         break
-#     orig = geocoding(loc1,key)
-#     loc2 = input("Destination: ")
-#     if loc2 == "quit" or loc2 == "q":
-#         break
-#     dest = geocoding(loc2,key)
-#     print("=================================================")
-#     if orig[0] == 200 and dest[0] == 200:
-#         op="&point="+str(orig[1])+"%2C"+str(orig[2])
-#         dp="&point="+str(dest[1])+"%2C"+str(dest[2])
-#         paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp
-#         paths_status = requests.get(paths_url).status_code
-#         paths_data = requests.get(paths_url).json()
-#         print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url)
-#         print("=================================================")
-#         print("Directions from " + orig[3] + " to " + dest[3] + " by " + vehicle)
-#         print("=================================================")
-#         if paths_status == 200:
-#             miles = (paths_data["paths"][0]["distance"])/1000/1.61
-#             km = (paths_data["paths"][0]["distance"])/1000
-#             sec = int(paths_data["paths"][0]["time"]/1000%60)
-#             min = int(paths_data["paths"][0]["time"]/1000/60%60)
-#             hr = int(paths_data["paths"][0]["time"]/1000/60/60)
-#             print("Distance Traveled: {0:.1f} miles / {1:.1f} km".format(miles, km))
-#             print("Trip Duration: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
-#             print("=================================================")
-#             for each in range(len(paths_data["paths"][0]["instructions"])):
-#                 path = paths_data["paths"][0]["instructions"][each]["text"]
-#                 distance = paths_data["paths"][0]["instructions"][each]["distance"]
-#                 print("{0} ( {1:.1f} km / {2:.1f} miles )".format(path, distance/1000, distance/1000/1.61))
-#             print("=================================================")
-#         else:
-#             print("Error message: " + paths_data["message"])
-#             print("*************************************************")
-# 
+client_data = get_ip_data()
+sources = generate_eonet_dictionaries(eonet_source_url, "sources")
+categories = generate_eonet_dictionaries(eonet_categories_url, "categories")
+
+if client_data != None:
+    query_url = generate_eonet_query(source="IRWIN,abfire,test",category="drought,wildfires",status="all",limit="20",start="2000-01-01",end=default_end_date,scale=20)
+    print(query_url)
+    print(json.dumps(get_eonet_data(query_url), indent=4))
+
+# Placeholder for GUI
